b9e2ca18683406bb17fee27388036e75b40c9892
Franco Culaciati <culaciati15@gmail.com>
2018-07-21 22:04:56 -0300

    Elimino RouteNumber

--- app/src/main/java/francotobias/tdpproyecto/Stop.java
+++ app/src/main/java/francotobias/tdpproyecto/Stop.java
@@ -5,4 +5,17 @@ import com.google.android.gms.maps.model.LatLng;
 public class Stop {
 	protected LatLng location;
 	protected boolean isGo;
+
+	public Stop(double lat, double lng, boolean go) {
+		location = new LatLng(lat, lng);
+		isGo = go;
+	}
+
+	public LatLng getLocation() {
+		return location;
+	}
+
+	public boolean isGo() {
+		return isGo;
+	}
 }
